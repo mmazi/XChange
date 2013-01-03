2c7c3978d224661965277ab95d3e3ba6310812b7
matija.mazi@gmail.com <matija.mazi@gmail.com>
2013-01-03 07:38:44 +0100

    Added CampBX's API use warnings

--- xchange-campbx/src/main/java/com/xeiam/xchange/campbx/api/CampBX.java
+++ xchange-campbx/src/main/java/com/xeiam/xchange/campbx/api/CampBX.java
@@ -31,7 +31,15 @@ import javax.ws.rs.Produces;
 
 /**
  * @author Matija Mazi <br/>
- * See https://campbx.com/api.php for up-to-date docs.
+ *         See https://campbx.com/api.php for up-to-date docs.
+ *         <p/>
+ *         Please heed the CampbBX's note:
+ *         <p/>
+ *         Important: Please note that using API and Website interfaces concurrently may cause login interference issues.
+ *         Please use different external IP addresses, or pause the bot when you need to use the Web UI.
+ *         <p/>
+ *         Please do not abuse the API interface with brute-forcing bots, and ensure that there is at least 500 millisecond
+ *         latency between two calls. We may revoke the API access without notice for accounts violating this requirement.
  */
 @Path("api")
 @Produces("application/json")
